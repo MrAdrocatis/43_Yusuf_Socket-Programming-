5eff4defce4ef6e84423633949a8250dafcc0df2
MrAdrocatis <mr.adrocatis@gmail.com>
2024-10-27 23:54:41 +0700

    Update
    nambah buat tugas web request

--- Web_request/obj/Debug/net8.0-windows/Web_request.AssemblyInfo.cs
+++ Web_request/obj/Debug/net8.0-windows/Web_request.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Tugas2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Web_request")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9816d0974dfedd776a48dc555ad7b2738b6da67")]
-[assembly: System.Reflection.AssemblyProductAttribute("Tugas2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Tugas2")]
+[assembly: System.Reflection.AssemblyProductAttribute("Web_request")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Web_request")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
